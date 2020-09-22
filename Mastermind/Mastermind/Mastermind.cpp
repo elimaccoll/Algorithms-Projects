dc53fb0d61c1ea2fb9cece7961e9c12c1c369355
Anthony Cherubino <cherubino.an@northeastern.edu>
2020-09-22 19:54:17 -0400

    Changed secret and guess to be two separate objects, numeric codes to be stored in vectors, and added error handling to fix crash on wrong number of values entered for a guess + other fixes (updated .gitignore to ignore VS files)

--- Mastermind/Mastermind/Mastermind.cpp
+++ Mastermind/Mastermind/Mastermind.cpp
@@ -1,106 +1,182 @@
 #include <iostream>
 #include <stdlib.h>
-#include <string>
+#include <vector>
+#include <sstream>
 using namespace std;
 
 class code							//program crashes when the guess contains the wrong number of digits
 {
+private:
+	int digits, range;
+	vector<int> numCode;
+
 public:
-	int n, m;
-	string sCode, guess;
-	bool win=false;
-
 	code(int, int);
-	string generateCode(int, int);
-	int checkCorrect(string);
-	int checkIncorrect(string);
+	void generateCode();
+	int inputCode(string);
+	int checkCorrect(code);
+	int checkIncorrect(code);
+	string formatCode();
 };
-    code::code(int digits, int range) {
-		n = digits;
-		m = range;
-		int trys = 0;
-		generateCode(n, m);
-		while (trys!=10 && win == false){
-			cout << "This is try #" << trys + 1<< endl;
-			cout << "Enter your guess: " << endl;
-			cin >> guess;
-			int cc = checkCorrect(guess);
-			int ci = checkIncorrect(guess);
-			cout << cc << "," << ci << endl;
-			trys++;
-			if (trys == 10) {
-				cout << "You failed the crack the code :(" << endl;
+
+code::code(int n, int m) {
+	digits = n;
+	range = m;
+}
+
+void code::generateCode() {
+	for (int i = 0; i < digits; i++) {
+		int codeVal = rand() % range;
+		numCode.push_back(codeVal);
+	}
+}
+
+int code::inputCode(string input) {
+	numCode.clear();
+	stringstream inputSS(input);
+	int num;
+	while (inputSS.good())
+	{
+		string substr;
+		getline(inputSS, substr, ',');
+		try {
+			numCode.push_back(stoi(substr));
+		}
+		catch (...) {
+			return -1;
+		}
+	}
+	if (numCode.size() != digits) {
+		return -2;
+	}
+	return 0;
+}
+
+int code::checkCorrect(code guess) {
+	int cc = 0;		//counter for check correct
+	for (int i = 0; i < digits; i++) {
+		if (guess.numCode[i] == numCode[i]) {
+			cc++;
+		}
+	}
+	return cc;
+}
+
+int code::checkIncorrect(code guess) {
+	vector<int> numCodeCopy = numCode;
+	int ci = 0;	//counter for check incorrect
+	for (int i = 0; i < digits; i++) {	//eliminates correct values in correct indicies
+		if (guess.numCode[i] == numCode[i]) {
+			guess.numCode[i] = 'e';
+			numCode[i] = 'e';
+		}
+	}
+	for (int i = 0; i < digits; i++) {
+		for (int j = 0; j < digits; j++) {
+			if (guess.numCode[i] == numCode[j] && guess.numCode[i] != 'e') {
+				ci++;
+				guess.numCode[i] = 'e';
+				numCode[j] = 'e';
 			}
 		}
 	}
+	numCode = numCodeCopy;
+	return ci;
+}
 
-	string code::generateCode(int digits, int range) {
-		for (int i = 0; i < digits; i++) {
-			int codeVal = rand() % range;
-			sCode += to_string(codeVal);
-			/*if (i != digits - 1) {					maybe put back in later but for now its messing up formatting 
-				sCode += to_string(codeVal) + ",";		makes code format: (1,2,3,4,5) but commas mess up for loop when comparing elements
-			}
-			else {
-				sCode += to_string(codeVal);
-			}*/
+string code::formatCode() {
+	string output = "(";
+	for (int i = 0; i < digits; i++) {
+		output += to_string(numCode[i]);
+		if (i != digits - 1)
+		{
+			output += ",";
 		}
-		cout << "Secret Code: "<< sCode << endl;	//prints secret code
-		return sCode;
 	}
+	output += ")";
+	return output;
+}
 
-	int code::checkCorrect(string g) {
-		int cc=0;		//counter for check correct
-		if (g.length() !=n) {
-			cout << "Guess has incorrect number of digits" << endl;
+int main()
+{
+	bool win = false;
+	int trys = 0;
+	int n, m;
+	string input;
+	bool repeat;
+	int err;
+
+	do {
+		cout << "Enter the amount of digits in the code: ";
+		cin >> n;
+		if (n <= 0) {
+			cout << "\nInvalid number of digits!\n\n";
+			repeat = true;
 		}
 		else {
-			for (int i = 0; i<n; i++) {
-				if (g[i] == sCode[i]) {
-					cc++;
-				}
-			}
+			repeat = false;
 		}
-		//cout << "Check Correct: " << cc << endl; //prints check correct return
+	} while (repeat);
+
+	do {
+		cout << "Enter the exclusive upper bound of digits in the code: ";
+		cin >> m;
+		if (m <= 0) {
+			cout << "\nInvalid upper bound!\n\n";
+			repeat = true;
+		}
+		else {
+			repeat = false;
+		}
+	} while (repeat);
+
+
+	code secret(n, m);
+	secret.generateCode();
+	code guess(n, m);
+	cout << "Code generated with (" << n << ") digits and a range of (0-" << m - 1 << ")" << endl;
+	
+	cout << "The \"secret\" code is: " << secret.formatCode() << endl;
+	
+	while (trys < 10 && win == false) {
+		cout << "\nYou have " << 10 - trys << " guesses remaining" << endl;
+		do {
+			cout << "Enter your guess (digits separated with commas): ";
+			cin >> input;
+			err = guess.inputCode(input);
+			if (err == -1) {
+				cout << "\nInvalid guess!\n\n";
+				repeat = true;
+			}
+			else if (err == -2) {
+				cout << "\nGuess has incorrect number of digits!\n\n";
+				repeat = true;
+			}
+			else {
+				repeat = false;
+			}
+		} while (repeat);
+
+		int cc = secret.checkCorrect(guess);
+		int ci = secret.checkIncorrect(guess);
+		cout << "\nMastermind: " << cc << "," << ci << endl;
+		
 		if (cc == n) {
-			cout << "You Cracked the Code!" << endl;
+			cout << "You cracked the code!" << endl;
 			win = true;
 		}
-		return cc;
-	}
-
-	int code::checkIncorrect(string g) {
-		string sCodeCopy=sCode;
-		int ci = 0;	//counter for check incorrect
-		for (int i = 0; i < n; i++) {	//eliminates correct values in correct indicies
-			if (g[i] == sCode[i]) {
-				g[i] = 'e';
-				sCode[i] = 'e';
+		else {
+			trys++;
+			if (trys == 10) {
+				cout << "You failed to crack the code :(" << endl;
 			}
 		}
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				if (g[i] == sCode[j] && g[i]!='e') {		
-					ci++;
-					g[i] = 'e';
-					sCode[j] = 'e';
-				}
-			}
-		}
-		sCode = sCodeCopy;
-		//cout << "Check Incorrect: " << ci << endl;	//prints check incorrect return
-		return ci;
+
 	}
 
-	int main()
-	{
-		int digits, range;
-		string guess;
-		cout << "Enter your number of digits: ";
-		cin >> digits;
-		cout << "Enter the upper bound of your range: ";
-		cin >> range;
 
-		code play(digits, range);
-		return 0;
-	}
+
+	cout << "The secret code was: " << secret.formatCode() << endl;	//prints secret code
+
+	return 0;
+}
